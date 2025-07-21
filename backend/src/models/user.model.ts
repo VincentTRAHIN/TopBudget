1aa15dff1f298008c9718a3cf7129f9e62b8cc37
VincentTrahin <vincent.trahin@gmail.com>
2025-07-21 15:38:09 +0200

    Feat: Replace bcrypt with bcryptjs for better compatibility

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { IUser, UserRole } from "../types/user.types";
 
 const UserSchema = new Schema<IUser>({
